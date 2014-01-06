3219362d2f511c6d55d756013c2309efcc425185
Greg Cerveny <greg.cerveny@gmail.com>
2014-01-06 13:56:53 -0600

    adding data to panel

--- logs.js
+++ logs.js
@@ -1,5 +1,8 @@
+var stdout = document.getElementById('stdout');
+stdout.innerHTML += 'Hello World\n';
+
 var socket = io.connect('http://localhost:5000');
 socket.on('news', function (data) {
-    console.log(data);
+    stdout.innerHTML += data.hello + '\n';    
     socket.emit('my other event', { my: 'data' });
 });
