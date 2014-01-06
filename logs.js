1e6889282f7fa843c8ecff2cb3551f948c69fc26
Greg Cerveny <greg.cerveny@gmail.com>
2014-01-06 14:13:31 -0600

    reading logs

--- logs.js
+++ logs.js
@@ -2,7 +2,6 @@ var stdout = document.getElementById('stdout');
 stdout.innerHTML += 'Hello World\n';
 
 var socket = io.connect('http://localhost:5000');
-socket.on('news', function (data) {
-    stdout.innerHTML += data.hello + '\n';    
-    socket.emit('my other event', { my: 'data' });
+socket.on('log', function (data) {
+    stdout.innerHTML += data + '\n';    
 });
